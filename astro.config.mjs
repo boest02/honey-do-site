8048279fefb023db136531706112a9600dc377de
Stephen Boettcher <sboettcher@macbookpro.myfiosgateway.com>
2025-10-06 08:02:13 -0400

    setup sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://honeydoheroes.netlify.app/",
   integrations: [
     sitemap(),
   ],
